7d6f9995abdfb09ae81356ba96a52973e2d3b430
Peyton Bechard <peyton.bechard@gmail.com>
2025-11-24 21:31:33 +0800

    Adds content to module 2 page

--- src/components/modules/Module2.tsx
+++ src/components/modules/Module2.tsx
@@ -1,5 +1,19 @@
 const Module2 = () => {
-  return <div>Coming soon...</div>;
+  return (
+    <>
+      <h2>Module 2.1.2: Laws and Policies that Affect Teaching</h2>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/X8zuZbhMF5o?si=_GbZtNHwuJ-Yq6Fg"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </>
+  );
 };
 
 export default Module2;

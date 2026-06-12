4ba87f4710b126d584f69f2b1ede4adb8575c0e9
Peyton Bechard <peyton.bechard@gmail.com>
2026-06-12 10:03:06 +0800

    Adds M3A2U2 video

--- src/components/modules/Module3.tsx
+++ src/components/modules/Module3.tsx
@@ -1,5 +1,20 @@
+import vid from "../../assets/M3U2A2 Video Compressed.mp4";
+
 const Module3 = () => {
-  return <div>Coming soon...</div>;
+  return (
+    <>
+      <h2>M3U2A2: Understanding Multilingual Learners</h2>
+      <iframe
+        width="560"
+        height="315"
+        src={vid}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen></iframe>
+    </>
+  );
 };
 
 export default Module3;
